f162da9634f6640722e7b91ce74a8156b99ce94a
islamqasimov <12495@holbertonstudents.com>
2026-02-04 09:34:11 -0500

    autocommit

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show --format=nt "$1" | cut -d: -f2 | sed '/^$/d; /password hashes cracked/d' > 5-password.txt
+john --format=nt --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show --format=nt "$1" | cut -d: -f2 | sed '/^$/d; /password hashes cracked/d' | head -n -1> 5-password.txt
