c258c61fedd72b0e3c4958782764573cbacdf2d1
hbtstudnet <12495@holbertonstudents.com>
2026-02-13 07:15:23 +0000

    autocommit

---
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+iptables -F; iptables -P INPUT DROP; iptables -P FORWARD DROP; iptables -P OUTPUT ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
