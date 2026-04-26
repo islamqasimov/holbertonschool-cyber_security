1394427e336f7a54138a77ce8278059e7be983d9
hbtstudnet <12495@holbertonstudents.com>
2026-04-26 15:17:29 +0000

    autocommit

--- network_security/0x07_nmap_post_port_scan_scripting/1-nmap_vulners.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/1-nmap_vulners.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    echo "Usage: $0 <target>"
-    exit 1
-fi
-
-echo "Initiating vulnerability scan on $1 (Ports 80, 443)..."
-sudo nmap -sV --script vulners -p 80,443 "$1"
+nmap --script vulners -p 80,443 $1
--- network_security/0x07_nmap_post_port_scan_scripting/2-vuln_scan.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/2-vuln_scan.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    echo "Usage: $0 <target>"
-    exit 1
-fi
-
-TARGET=$1
-OUTPUT_FILE="vuln_scan_results_${TARGET}.txt"
-
-echo "Checking for CVE-2017-5638 on $TARGET..."
-echo "Results will be saved to $OUTPUT_FILE"
-
-sudo nmap -p 80,443,8080 --script http-vuln-cve2017-5638 "$TARGET" -oN "$OUTPUT_FILE"
-
-echo "Scan complete. Check $OUTPUT_FILE for details."
+nmap --script http-vuln-cve2017-5638 $1 -oN vuln_scan_results.txt
